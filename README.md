505fc65777bf54cde54d5c57607fefaac7726d31
Adrian <322735+adrian-schnell@users.noreply.github.com>
2022-02-02 11:36:27 +0100

    init repo

---
+++ README.md
@@ -0,0 +1,23 @@
+# DeFiChain Donation Bot
+
+The task of this bot is to inform about incoming funds on a defined DFI address as tweet.
+
+
+# Setup
+
+* create own config.json with `cp config_example.json config.json` and add own values
+* Run the `setup.py` and follow the instructions.
+
+	- script will output the auth url for twitter
+	- login to twitter and give auth access
+	- enter the `PIN` in the setup process
+	- copy/paste the `access_token` and `access_token_secret` to the `config.json`
+
+* To monitor your DFI address, enter your DFI address in the `config.json` (attribute: `address`)
+
+* In the config, you can edit the text of the tweet (attribute: `tweet_settings-base_text`)
+
+* to test the script, you can disable the `send_tweet` flag and run the script (`python3 bot.py`)
+
+* to run the bot periodically, you should add a cronjob to trigger it (e.g. hourly):
+`0 * * * * python3 bot.py > bot_activity.log`
---
+++ bot.py
@@ -0,0 +1,139 @@
+import requests
+import json
+import tweepy
+import os
+
+def parse_ocean_token(address):
+    retval = {}
+    request = requests.get(f'https://ocean.defichain.com/v0/mainnet/address/{address}/tokens')
+    request.raise_for_status()
+
+    if not request.ok:
+        exit(f'Error {request.status_code} in token request')
+
+    for i in request.json()['data']:
+        retval[i['symbol']] = {
+            'symbol': i['symbol'],
+            'symbolKey': i['symbolKey'],
+            'amount': float(i['amount'])
+        }
+
+    return retval
+
+
+def parse_ocean_utxo(address):
+    retval = {}
+    request = requests.get(f'https://ocean.defichain.com/v0/mainnet/address/{address}/balance')
+    request.raise_for_status()
+
+    if not request.ok:
+        exit(f'Error {request.status_code} in utxo request')
+
+    retval['DFI'] = {
+        'symbol': 'DFI',
+        'symbolKey': 'DFI',
+        'amount': float(request.json()['data'])
+    }
+
+    return retval
+
+
+def merge_token_utxo(utxo, token):
+    larger_set = utxo if len(utxo) > len(token) else token
+    smaller_set = token if len(token) < len(utxo) else utxo
+
+    for k, v in smaller_set.items():
+        if k in larger_set:
+            larger_set[k]['amount'] += v['amount']
+        else:
+            larger_set[k]['amount'] = v['amount']
+
+    return larger_set
+
+
+def load_data(datafile):
+    try:
+        with open(datafile, 'r') as fp:
+            prevData = json.load(fp)
+    except IOError as e:
+        print(e)
+        prevData = {}
+    
+    return prevData
+
+
+def save_data(datafile, data):
+    # Store into tmp file
+    with open(datafile, 'w+') as f:
+        json.dump(data, f)
+
+
+def get_diff(new, old):
+    retval = []
+    for k, v in new.items():
+        if k in old:
+            if v['amount'] > old[k]['amount']:
+                difference = v['amount'] - old[k]['amount']
+                if k.find('-') != -1:
+                    print('skipping LM token: ' + k)
+                    continue
+                if k == 'DFI' and difference < 1:
+                    print('DFI amount below 1, skipping: {:.8f}'.format(difference))
+                    continue
+                retval.append((k, difference))
+        else:
+            retval.append((k, v['amount']))
+    return retval
+
+
+def get_message(diff, address, base_text, include_defiscan_link):
+    message = ''
+    if len(diff) > 0:
+        message = base_text + '\n'
+        for coin, amount in diff:
+            message += f'{amount:.8f} {coin}\n'
+
+        if include_defiscan_link:
+            message += '\nsee all details: https://defiscan.live/address/' + address
+
+    return message
+
+
+def twitter_client(config):
+    auth = tweepy.OAuthHandler(config['twitter']['consumer_key'], config['twitter']['consumer_secret'])
+    auth.set_access_token(config['twitter']['access_token'], config['twitter']['access_token_secret'])
+    
+    return tweepy.API(auth)
+
+
+def send_tweet(message, config):
+    api = twitter_client(config)
+    try:
+        if len(message) > 0:
+            api.update_status(status=message)
+    except tweepy.errors.TweepyException as e:
+        print(e)
+        print('error while sending the tweet...')
+
+
+def main():
+    config = load_data('{}/config.json'.format(os.getcwd()))
+
+    dataToken = parse_ocean_token(config['address'])
+    dataUTXO = parse_ocean_utxo(config['address'])
+    data = merge_token_utxo(dataUTXO, dataToken)
+    prevData = load_data(config['datafile'])
+    save_data(config['datafile'], data)
+    diff = get_diff(data, prevData)
+    message = get_message(diff, config['address'], config['tweet_settings']['base_text'], config['tweet_settings']['include_defiscan_link'])
+
+    if config['verbose'] and len(message) > 0:
+        print('will tweet message:\n' + message)
+
+    if config['send_tweet'] and len(message) > 0:
+        send_tweet(message, config)
+        print('tweet sent with message\n'+message)
+
+
+if __name__ == "__main__":
+    main()
